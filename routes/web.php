8b89ccdce05f3deb5df235feccf4d2bc43edc04b
bekturov <190103322@stu.sdu.edu.kz>
2021-03-18 02:06:44 +0600

    add main

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', function () {
+    return view('main');
+})->name('main');
+
+Route::get('/page2', function () {
+    return view('page2');
+})->name('page2');
+
+Route::get('/page3', function () {
+    return view('page3');
+})->name('page3');
