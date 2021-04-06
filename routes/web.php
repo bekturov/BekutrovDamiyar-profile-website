efcb6bc73eccd2a46c30cfa0468993578747f08c
bekturov <190103322@stu.sdu.edu.kz>
2021-04-07 01:21:57 +0600

    create post index page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Http\Controllers\BlogController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,7 +33,5 @@ Route::get('/post/add', function () {
     ]);
 });
 
-Route::get('/post', function () {
-    $post = Post::find(1);
-    return $post;
-});
+Route::get('/post', [BlogController::class, 'index']);
+
